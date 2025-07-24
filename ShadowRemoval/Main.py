4916f35d806784dbc03e5b473f94e60ad48fd74b
Trisha1818 <chintalatrisha@gmail.com>
2025-07-24 12:02:26 +0530

    Update Main.py

--- ShadowRemoval/Main.py
+++ ShadowRemoval/Main.py
@@ -44,7 +44,7 @@ hard_mask_raw = cv2.imread(hard_mask_path)
 if hard_mask_raw is None:
     raise FileNotFoundError(f"Could not load hard mask image: {hard_mask_path}")
 
-hard_mask = cv2.cvtColor(hard_mask_raw, cv2.COLOR_BGR2RGB).astype('double') / 255.0
+hard_mask = cv2.cvtColor(hard_mask_raw, cv2.COLOR_BGR2RGB).astype('float32') / 255.0
 hard_mask = cv2.resize(hard_mask, (hard_mask.shape[1] // 2, hard_mask.shape[0] // 2))
 
 plt.imshow(hard_mask)
